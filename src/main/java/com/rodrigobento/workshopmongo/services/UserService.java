138938b1f1be79adfdba99c37f554c641390452d
Rodrigo <rodrigo.bento2010@gmail.com>
2025-09-24 13:33:48 -0300

    Inserção de usuário com POST

--- src/main/java/com/rodrigobento/workshopmongo/services/UserService.java
+++ src/main/java/com/rodrigobento/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigobento.workshopmongo.domain.User;
+import com.rodrigobento.workshopmongo.dto.UserDTO;
 import com.rodrigobento.workshopmongo.repository.UserRepository;
 import com.rodrigobento.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -25,5 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
     
-    
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
 }
