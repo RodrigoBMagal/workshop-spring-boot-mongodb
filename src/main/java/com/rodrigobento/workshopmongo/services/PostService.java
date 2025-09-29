da5068e3344b70f9512a8adc5cc788766321d6d9
Rodrigo <rodrigo.bento2010@gmail.com>
2025-09-29 13:46:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/rodrigobento/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.rodrigobento.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigobento.workshopmongo.domain.Post;
+import com.rodrigobento.workshopmongo.domain.User;
+import com.rodrigobento.workshopmongo.dto.UserDTO;
+import com.rodrigobento.workshopmongo.repository.PostRepository;
+import com.rodrigobento.workshopmongo.repository.UserRepository;
+import com.rodrigobento.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    
+}
